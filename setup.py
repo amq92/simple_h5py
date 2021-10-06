7d4914d3dc4070518fdc231a4407af0c4eb04372
Arturo Mendoza <amq92@users.noreply.github.com>
2021-10-07 01:14:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='simple_h5py',
-    version='0.1.1',
+    version='0.1.2',
     author='Arturo Mendoza',
     description='A simple wrapper for the h5py library.',
     long_description=long_description,
